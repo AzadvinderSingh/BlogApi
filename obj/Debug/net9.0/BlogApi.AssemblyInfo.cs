b86ba64664fe6558c47f6e80a5a3791c199813f2
AzadvinderSingh <azadvindersingh80@gmail.com>
2026-08-28 19:30:07 +0530

    update

--- obj/Debug/net9.0/BlogApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlogApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c009c9ea4a5b6ea81dedf618bd11ee01d795fd4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+721be6d017e896542ab025d7dc7583c02e5f96f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
