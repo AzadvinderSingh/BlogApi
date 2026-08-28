62c6cd3c627267e4396a6611ce7581ad20b217f3
Azadvinder Singh <86160599+AzadvinderSingh@users.noreply.github.com>
2026-08-28 18:37:15 +0530

    Update BlogPost.cs

--- Models/BlogPost.cs
+++ Models/BlogPost.cs
@@ -1,37 +1,25 @@
-using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace BlogApi.Models
 {
-    // BlogPost class
     public class BlogPost
     {
-        public int Id { get; set; } 
-        public string Title { get; set; } 
-        public string Author { get; set; }
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
         public int Year { get; set; }
     }
 
-    // User class
     public class User
     {
-        public int Id { get; set; } 
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
     }
 
-    // UserCredential class - for login/register requests
     public class UserCredential
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
-
-        // You can add a constructor if needed, but it's optional
-        public UserCredential() { }
-
-        public UserCredential(string username, string password)
-        {
-            Username = username ?? throw new ArgumentNullException(nameof(username));
-            Password = password ?? throw new ArgumentNullException(nameof(password));
-        }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
     }
 }
